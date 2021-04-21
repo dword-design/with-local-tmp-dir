381fc1ebf2b8fc5863762e0bd457f8514871d30f
dword <info@dword-design.de>
2021-04-21 02:14:23 +0000

    fix: remove path argument and add some doc

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,16 +5,29 @@ import P from 'path'
 import self from '.'
 
 export default {
-  'async function': () =>
-    self(async () => {
-      const path1 = process.cwd()
+  'async function': async () => {
+    let path1
+    let path2
+    await self(async () => {
+      path1 = process.cwd()
       await new Promise(resolve => setTimeout(resolve, 500))
-
-      const path2 = process.cwd()
-      expect(path1).toEqual(path2)
-      expect(P.basename(path1).startsWith('tmp-')).toBeTruthy()
-      expect(P.basename(path2).startsWith('tmp-')).toBeTruthy()
-    }),
+      path2 = process.cwd()
+    })
+    expect(path1).toEqual(path2)
+    expect(P.basename(path1).startsWith('tmp-')).toBeTruthy()
+    expect(P.basename(path2).startsWith('tmp-')).toBeTruthy()
+  },
+  dir: async () => {
+    const cwd = process.cwd()
+    await ensureDir('foo')
+    let innerCwd
+    await self(() => (innerCwd = process.cwd()), {
+      dir: 'foo',
+    })
+    expect(P.basename(innerCwd).startsWith('tmp-')).toBeTruthy()
+    expect(P.dirname(innerCwd)).toEqual(P.join(cwd, 'foo'))
+    await remove('foo')
+  },
   'error: async': async () => {
     const cwd = process.cwd()
     let path
@@ -42,6 +55,8 @@ export default {
     expect(await exists(path)).toBeFalsy()
     expect(process.cwd()).toEqual(cwd)
   },
+  'no callback': () =>
+    expect(self).toThrow('Callback is required for with-local-tmp-dir'),
   'non-empty': async () => {
     let path
     let innerFileExists = false
@@ -53,26 +68,37 @@ export default {
     expect(innerFileExists).toBeTruthy()
     expect(await exists(path)).toBeFalsy()
   },
-  path: async () => {
-    const cwd = process.cwd()
-    await ensureDir('foo')
-    self('foo', () => {
-      expect(P.basename(process.cwd()).startsWith('tmp-')).toBeTruthy()
-      expect(P.dirname(process.cwd())).toEqual(P.join(cwd, 'foo'))
-    })
-    await remove('foo')
+  prefix: async () => {
+    let path
+    await self(() => (path = process.cwd()), { prefix: 'foo' })
+    expect(P.basename(path).startsWith('foo-')).toBeTruthy()
+    expect(P.dirname(path)).toEqual(process.cwd())
   },
   simple: async () => {
     const cwd = process.cwd()
     let path
-    await self(() => {
-      expect(P.basename(process.cwd()).startsWith('tmp-')).toBeTruthy()
-      expect(P.dirname(process.cwd())).toEqual(cwd)
-      path = process.cwd()
-    })
+    await self(() => (path = process.cwd()))
     expect(process.cwd()).toEqual(cwd)
-    expect(path).toBeDefined()
+    expect(P.basename(path).startsWith('tmp-')).toBeTruthy()
+    expect(P.dirname(path)).toEqual(cwd)
     expect(await exists(path)).toBeFalsy()
+  },
+  'unsafe cleanup and dir': async () => {
+    let path
+    await ensureDir('foo')
+    await expect(
+      self(
+        async () => {
+          await outputFile('test.txt', '')
+          path = process.cwd()
+        },
+        { dir: 'foo', unsafeCleanup: false }
+      )
+    ).rejects.toThrow()
+    expect(P.dirname(path)).toEqual(P.resolve('foo'))
+    expect(await exists(path)).toBeTruthy()
+    await remove(path)
+    await remove('foo')
   },
   'unsafe cleanup cwd': async () => {
     let path
@@ -87,23 +113,5 @@ export default {
     ).rejects.toThrow()
     expect(await exists(path)).toBeTruthy()
     await remove(path)
-  },
-  'unsafe cleanup path': async () => {
-    let path
-    await ensureDir('foo')
-    await expect(
-      self(
-        'foo',
-        async () => {
-          await outputFile('test.txt', '')
-          path = process.cwd()
-        },
-        { unsafeCleanup: false }
-      )
-    ).rejects.toThrow()
-    expect(P.dirname(path)).toEqual(P.resolve('foo'))
-    expect(await exists(path)).toBeTruthy()
-    await remove(path)
-    await remove('foo')
   },
 }
